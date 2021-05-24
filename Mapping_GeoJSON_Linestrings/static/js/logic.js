87b14bdd73f23ebd07925c4025c4119890d54c06
Jeffrey Au <jeffaupc@gmail.com>
2021-05-24 17:54:36 -0400

    night toronto

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -1,11 +1,11 @@
 // We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let night = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let day = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-day-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -13,15 +13,15 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps
 let baseMaps = {    
-    Light: light,
-    Dark: dark
+    Day: day,
+    Night: night
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
     center: [44.0, -80.0],
     zoom: 2,
-    layers: [light]
+    layers: [night]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -32,7 +32,11 @@ let torontoData = "https://raw.githubusercontent.com/jeffreyaupc/Mapping_Earthqu
 
 // Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
-    data.features.forEach(x => L.geoJson(x)
+    data.features.forEach(x => L.geoJson(x, {
+        color: "yellow",
+        lineweight: 2,
+        opacity: 0.7
+    })
     .bindPopup(`<h2>Airline: ${x.properties.airline}</h2>
     <hr><h3>Destination: ${x.properties.dst}</h3>`)
     .addTo(map))
